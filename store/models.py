8a643d826b6a212e2864de3ed34171e8ddc87ca5
Lycoronosis <90725229+Lycoronosis@users.noreply.github.com>
2024-11-17 22:27:53 +0100

    Added database models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+# Model for generic user
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    is_admin = models.BooleanField(default=False)
+
+
+# Model for products on website
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    catalog_code = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+# Model for products parameters to add later through form
+class ProductParameter(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='parameters')
+    key = models.CharField(max_length=100)
+    value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.key}: {self.value}"
+    
+
+# Function for uploading images to product folder
+def product_image_upload_path(instance, filename):
+    return f"products/{instance.product.id}/images/{filename}"
+
+
+# Model for product images
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name= 'images')
+    image = models.ImageField(upload_to= product_image_upload_path)
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
+    
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    guest_email = models.EmailField(null=True, blank=True)
+    guest_phone = models.CharField(max_length=15, null=True, blank=True)
+    address = models.TextField()
+    is_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id}"
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product}"
+    
